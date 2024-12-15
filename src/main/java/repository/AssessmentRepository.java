f2e651a7caf5954769b7fc7abec3811a2d474944
Nada-ibrahim12 <123187920+Nada-ibrahim12@users.noreply.github.com>
2024-12-16 00:21:14 +0200

    .

--- src/main/java/repository/AssessmentRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-import model.Assessment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssessmentRepository extends JpaRepository<Assessment, Long> {
-
-}
--- src/main/java/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-import model.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-
-}
