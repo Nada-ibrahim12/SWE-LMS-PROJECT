7427a0ea59dcc9260c85521da42d75da67e97384
Nada-ibrahim12 <123187920+Nada-ibrahim12@users.noreply.github.com>
2024-12-15 12:36:16 +0200

    Structure

---
+++ src/main/java/org/example/learning_managment_system/LearningManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.learning_managment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import repository.UserRepository;
+
+@SpringBootApplication
+public class  LearningManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningManagmentSystemApplication.class, args);
+    }
+}
