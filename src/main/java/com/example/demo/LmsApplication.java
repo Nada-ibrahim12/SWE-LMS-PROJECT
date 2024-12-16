b78018f6aa6da43534d059af35b92e8ce67e78e6
george <georgesheruf1234@gmail.com>
2024-12-16 06:38:53 +0200

    init

--- src/main/java/com/example/demo/LmsApplication.java
+++ src/main/java/com/example/demo/LmsApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LmsApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(LmsApplication.class, args);
+
     }
 }
