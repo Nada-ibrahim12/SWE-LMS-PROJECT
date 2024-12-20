04d516303b7db353db4e59e81d1e0a4bc09fbc4a
george <georgesheruf1234@gmail.com>
2024-12-20 05:39:07 +0200

    fixing pom files

--- src/main/java/com/example/demo/LmsApplication.java
+++ src/main/java/com/example/demo/LmsApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableAdminServer
 
 public class LmsApplication {
 
